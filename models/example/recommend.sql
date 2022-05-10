7b7f2af4257c3e6e260ceb529258455b47ae749c
goeld7898@gmail.com <goeld7898@gmail.com>
2022-05-10 08:33:22 +0000

    recommendation updated

--- models/example/recommend.sql
+++ models/example/recommend.sql
@@ -1,4 +1,4 @@
-select course_name, categories, tenants, course_start_year, demo_taken, course_purchased
+select *
 from {{ref('postgres_courses')}} 
-full join {{ref('postgres_recommend')}} 
+inner join {{ref('postgres_recommend')}} 
 on {{ref('postgres_courses')}}.difficulty_level = {{ref('postgres_recommend')}}.dif
